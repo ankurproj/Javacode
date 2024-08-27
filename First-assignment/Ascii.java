a99faa57d0e85653509c4883822d56368804dbb0
ANKUR KUMAR <155262565+ankurproj@users.noreply.github.com>
2024-08-27 11:55:54 +0530

    Done all programs of first assignment

---
+++ First-assignment/Ascii.java
@@ -0,0 +1,7 @@
+//Q2 - Print the ASCII value of character ‘U’.
+public class Ascii {
+    public static void main(String[] args) {
+        int x = 'U';
+        System.out.println("Ascii value of U is "+ x);
+    }
+}
